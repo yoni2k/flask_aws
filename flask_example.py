df71fd5302ec88f6c80a622a05b0cc7e48c30300
Yoni Krichevsky <yoni.kr2@gmail.com>
2023-01-12 16:41:14 +0200

    Change to October 2020

--- flask_example.py
+++ flask_example.py
@@ -2,18 +2,13 @@ from flask import Flask
 
 example_app = Flask(__name__)
 
-LOCAL_RUN = False
 AWS_PORT = 8080
 
 
 @example_app.route('/')
 def hello_world():
-    return 'Hello Fellows June 2022.  Are you excited for the Hackathon?'
+    return 'Hello Fellows October 2022.  Are you excited for the Hackathon?'
 
 
 if __name__ == '__main__':
-
-    if LOCAL_RUN:
-        example_app.run()
-    else:
-        example_app.run(host='0.0.0.0', port=AWS_PORT)
+    example_app.run(host='0.0.0.0', port=AWS_PORT)
